be35e1881d0f0fd3bd486e6baf3fe94591496974
Progamador <jmj-prog@gmail.com>
2015-12-21 20:40:40 -0200

    add migration

--- migrations/20151221192556-unnamed-migration.js
+++ migrations/20151221192556-unnamed-migration.js
@@ -2,13 +2,34 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+      migration.createTable(
+          'comprador',
+          {
+              idComprador: {
+                  type: DataTypes.INTEGER,
+                  primaryKey: true,
+                  autoIncrement: true
+              },
+              createdAt: {
+                  type: DataTypes.DATE
+              },
+              updatedAt: {
+                  type: DataTypes.DATE
+              },
+              idEmpresa: {
+                  type: DataTypes.INTEGER,
+                  required: true
+              },
+              idLeilao: {
+                  type: DataTypes.idLeilao,
+                  required: true
+              }
+          },
+          {
+              engine: 'MYISAM', // default: 'InnoDB'
+              charset: 'latin1' // default: null
+          }
+      )
   },
 
   down: function (queryInterface, Sequelize) {
